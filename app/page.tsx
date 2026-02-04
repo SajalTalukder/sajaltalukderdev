7bd3c4daf5f70d00b83ae07828f40dcfe67f639b
Sajal Talukder <sajaltalukder01@gmail.com>
2026-02-04 11:15:55 +0600

    responsivefix

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen overflow-hidden">
       <Navbar />
       <HeroSection />
       <AboutSection />
